cd198b2c79e738605d37175b6fac8b3814365d9a
Thomas Pfau <github@thomaspfau.de>
2023-11-22 10:48:48 +0200

    Adding compose tester and nginx config

--- FastChat/start_FastChat.sh
+++ FastChat/start_FastChat.sh
@@ -4,15 +4,15 @@
 echo "Starting Controller"
 python3 -m fastchat.serve.controller --host 0.0.0.0 --port 21001 &
 #Wait till it's up
-sleep 5
+sleep 3
 # Then, launch the model worker(s)
 echo "Loading Model $MODEL"
 python3 -m fastchat.serve.model_worker --model-path $MODEL --device cpu --controller-address http://127.0.0.1:21001 --host 0.0.0.0 --port 21002 &
 # Wait for the model to be loaded
-sleep 20
+
 # Finally, launch the RESTful API server
 echo "Starting Server on $HOST:$PORT"
-python3 -m fastchat.serve.openai_api_server --host $HOST --port $PORT --controller-address http://127.0.0.1:21001 &
+python3 -m fastchat.serve.openai_api_server --host $HOST --port $PORT --controller-address http://127.0.0.1:21001 
 
 # Keep script running (this feels like a hack. but if it works...)
-tail -f /dev/null
+#tail -f /dev/null
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 This repository will contain all set up and Dockerfile routines for the LLM deployment.
 Each subfolder contains a type of pod that should be deployable along with some instructions for it.
+
+The nginxPod folder contains some settings for the up-front nginx server, which will in the end perform an Auth check.
+The compose_test is a simple testing approach for the interaction between the nginx pod and the FastChat.
