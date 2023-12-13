94d643f3d56cbd062bbaf65bb734e412c7238b02
Thomas Pfau <github@thomaspfau.de>
2023-12-13 08:08:09 +0200

    Adding GPU settings

---
+++ llama-cpp-python/start_server_gpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Start controller
+echo "Starting Server"
+env
+python3 -m llama_cpp.server --host $HOST --model /models/$MODEL --port $PORT --chat_format $CHATFORMAT --n_gpu_layers 35
+
+
