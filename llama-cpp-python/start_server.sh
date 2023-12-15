25e0fa69c3269c211ac09d9c271cd06e0b2f2e5e
Thomas Pfau <github@thomaspfau.de>
2023-12-15 10:08:31 +0200

    Adding Helm templating

--- llama-cpp-python/start_server.sh
+++ llama-cpp-python/start_server.sh
@@ -3,4 +3,6 @@
 # Start controller
 echo "Starting Server"
 env
-python3 -m llama_cpp.server --host $HOST --model /models/$MODEL --port $PORT --chat_format $CHATFORMAT
+python3 -m llama_cpp.server --host $HOST --model /models/$MODEL --port $PORT --chat_format $CHATFORMAT --n_threads $N_CPUS
+
+
