9760a77eba19a41e35f0a48c4010a9ad774396b1
Thomas Pfau <github@thomaspfau.de>
2024-04-26 10:43:32 +0300

    updating push targets

--- updateDocker.sh
+++ updateDocker.sh
@@ -3,7 +3,7 @@
 ## Builds the docker image and pushes it to harbor. Needs to be logged in to harbor to be able to do this.
 cd llama-cpp-python
 docker build --build-arg llama_cpp_version=$1 --file ./Dockerfile_gpu -t harbor.cs.aalto.fi/aaltorse-public/llm_llmcpp_gpu:$1 .
-docker push harbor.cs.aalto.fi/aaltorse-public/llm_llama_cpp_gpu
+docker push harbor.cs.aalto.fi/aaltorse-public/llm_llamacpp_gpu
 docker build --build-arg llama_cpp_version=$1 --file ./Dockerfile -t harbor.cs.aalto.fi/aaltorse-public/llm_llamacpp_cpu:$1 .
-docker push harbor.cs.aalto.fi/aaltorse-public/llm_llama_cpp
+docker push harbor.cs.aalto.fi/aaltorse-public/llm_llamacpp_cpu
 cd -
