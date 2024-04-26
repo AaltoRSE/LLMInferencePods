158881fb1ac7afd3be99af0f3ec2f57b253b16aa
Thomas Pfau <github@thomaspfau.de>
2024-04-26 10:37:15 +0300

    Updating docker again

--- updateDocker.sh
+++ updateDocker.sh
@@ -2,8 +2,8 @@
 
 ## Builds the docker image and pushes it to harbor. Needs to be logged in to harbor to be able to do this.
 cd llama-cpp-python
-docker build --file ./Dockerfile_gpu -t harbor.cs.aalto.fi/aaltorse-public/llm_llama_cpp_gpu .
+docker build --build-arg llama_cpp_version=$1 --file ./Dockerfile_gpu -t harbor.cs.aalto.fi/aaltorse-public/llm_llmcpp_gpu:$1 .
 docker push harbor.cs.aalto.fi/aaltorse-public/llm_llama_cpp_gpu
-docker build --file ./Dockerfile -t harbor.cs.aalto.fi/aaltorse-public/llm_llama_cpp .
+docker build --build-arg llama_cpp_version=$1 --file ./Dockerfile -t harbor.cs.aalto.fi/aaltorse-public/llm_llamacpp_cpu:$1 .
 docker push harbor.cs.aalto.fi/aaltorse-public/llm_llama_cpp
 cd -
