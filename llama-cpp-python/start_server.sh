586db606951e55d1c7ccfb4cf623079b4a8f8318
Thomas Pfau <github@thomaspfau.de>
2024-05-10 12:08:51 +0300

    Reverting context length setting usage

--- llama-cpp-python/start_server.sh
+++ llama-cpp-python/start_server.sh
@@ -3,6 +3,6 @@
 # Start controller
 echo "Starting Server"
 env
-python3 -m llama_cpp.server --host $HOST --model /models/$MODEL --port $PORT --chat_format $CHATFORMAT --n_threads $N_CPUS -n_ctx $N_CTX
+python3 -m llama_cpp.server --host $HOST --model /models/$MODEL --port $PORT --chat_format $CHATFORMAT --n_threads $N_CPUS
 
 
