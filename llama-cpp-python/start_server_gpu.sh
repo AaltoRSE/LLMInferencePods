0cd3c1add420faad2dccb76f8ce14da0474b9fae
Thomas Pfau <github@thomaspfau.de>
2024-05-10 12:25:47 +0300

    Re-Adding the context length, with the proper setting

--- llama-cpp-python/start_server_gpu.sh
+++ llama-cpp-python/start_server_gpu.sh
@@ -3,4 +3,4 @@
 # Start controller
 echo "Starting Server"
 env
-python3 -m llama_cpp.server --n_gpu_layers -1 --host $HOST --model /models/$MODEL --port $PORT --chat_format $CHATFORMAT --n_threads $N_CPUS
+python3 -m llama_cpp.server --n_gpu_layers -1 --host $HOST --model /models/$MODEL --port $PORT --chat_format $CHATFORMAT --n_threads $N_CPUS --n_ctx $N_CTX
