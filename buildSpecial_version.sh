106a69a67b8ccf9b05e618d0d2b1d9e699d15642
Thomas Pfau <github@thomaspfau.de>
2024-10-01 08:32:26 +0300

    Building special docker version

---
+++ buildSpecial_version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Builds the docker image and pushes it to harbor. Needs to be logged in to harbor to be able to do this.
+cd llama-cpp-python
+docker build --file ./Dockerfile_gpu -t harbor.cs.aalto.fi/aaltorse-public/llm_llamacpp_$1 .
+docker push harbor.cs.aalto.fi/aaltorse-public/llm_llamacpp_$1
+docker build --file ./Dockerfile -t harbor.cs.aalto.fi/aaltorse-public/llm_llamacpp_$1 .
+docker push harbor.cs.aalto.fi/aaltorse-public/llm_llamacpp_$1
+cd -
+
